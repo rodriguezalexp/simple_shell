cfe33f9ecb589b9b14166a32d612ff124ce6b827
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-04-15 04:33:24 +0000

    func

--- funtions.c
+++ funtions.c
@@ -64,3 +64,27 @@ size_t _countws(char *str)
 	}
 	return (counter);
 }
+
+/**
+ * _strdup - returns pointer to new mem alloc space which contains copy
+ * @strtodup: string to be duplicated
+ * Return: a pointer to the new duplicated string
+ */
+char *_strdup(char *strtodup)
+{
+	char *copy;
+
+	int len, i;
+
+	if (strtodup == 0)
+		return (NULL);
+
+	for (len = 0; strtodup[len]; len++)
+		;
+	copy = malloc((len + 1) * sizeof(char));
+
+	for (i = 0; i <= len; i++)
+		copy[i] = strtodup[i];
+
+	return (copy);
+}
